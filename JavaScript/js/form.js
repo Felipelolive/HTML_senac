94f563925a202df1ce0e2e576230973a2227c320
Felipelolive <felipeloliveira27@senacsp.edu.br>
2024-03-21 17:35:55 -0300

    aulas

---
+++ JavaScript/js/form.js
@@ -0,0 +1,14 @@
+salvo.addEventListener('click', function () {
+let enviar = window.document.getElementById('enviar');
+let nome = document.getElementById('nome')
+let email = document.getElementById('e-mail')
+let telefone = document.getElementById('telefone')
+let assunto = document.getElementById('assunto')
+let mensagem = document.getElementById('mensagem')
+let btnenviar = document.getElementById("btnenviar")
+});
+
+if (nomeValue) {
+    document.getElementById('nome2').value = nome;
+}
+ 
