a791c698ab615dfc3c25b9780c10dae352cd5a0e
Felipelolive <felipeloliveira27@senacsp.edu.br>
2024-04-03 16:11:09 -0300

    aulas

--- JavaScript/cadastroFor/assets/js/aula07.js
+++ JavaScript/cadastroFor/assets/js/aula07.js
@@ -1,5 +1,5 @@
 let vetor = []
-for (let posicao = 0;posicao<vetor.length; posicao++){
+for (let posicao = 0; posicao < vetor.length; posicao++) {
     console.log(vetor[posicao])
 }
 
@@ -11,16 +11,48 @@ let telefone = [];
 let assunto = [];
 let msg = [];
 //inserindo dados no arrays
-formcontato.addEventListener("submit", (event) =>{
+formcontato.addEventListener("submit", (event) => {
     event.preventDefault();
-    nome.push(formContato.nome.value);
-    email.push(formContato.email.value);
-    telefone.push(formContato.telefone.value);
-    assunto.push(formContato.assunto.value);
-    msg.push(formContato.msg.value);
-    gerarLista()
+
+    nome.push(formcontato.nome.value);
+    email.push(formcontato.email.value);
+    telefone.push(formcontato.telefone.value);
+    assunto.push(formcontato.assunto.value);
+    msg.push(formcontato.mensagem.value);
+    gerarLista();
 })
 
-function gerarLista(){
-    
+function gerarLista() {
+    let auxLista = "";
+
+    for (let posicao = 0; posicao < nome.length; posicao++) {
+        auxLista += `<div class="card">
+                        <div class="dados-label">
+                            <div>Nome completo:</div>
+                            <div>${nome[posicao]}</div>
+                        </div>
+                           
+                         <div class="dados-label">
+                             <div>E-mail:</div>
+                             <div>${email[posicao]}</div>
+                         </div>
+
+                        <div class="dados-label">
+                             <div>Telefone:</div>
+                             <div>${telefone[posicao]}</div>
+                        </div>
+
+                        <div class="dados-label">
+                            <div>Assunto:</div>
+                            <div>${assunto[posicao]}</div>
+                        </div>
+
+                        <div class="dados-label">
+                            <div>Mensagem:</div>
+                            <div>${msg[posicao]}</div>
+                         </div>
+                    </div>`
+                    
+    }
+    divLista.innerHTML = auxLista;
 }
