4f6255d871a648e36c3835d59325f2d1a77b6970
Felipelolive <felipeloliveira27@senacsp.edu.br>
2024-04-03 17:17:39 -0300

    aulas

---
+++ JavaScript/js/exercicio.js
@@ -0,0 +1,8 @@
+let valorContador = 0;
+ 
+const displayContador = document.getElementById('valorContador');
+const botaoDiminuir = document.getElementById('decrementar');
+const botaoResetar = document.getElementById('resetar');
+const botaoAumentar = document.getElementById('incrementar');
+ 
+function 
